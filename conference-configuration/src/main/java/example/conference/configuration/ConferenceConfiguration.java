9f34de7ee3750ec774e2f9354203c4315f8c4414
Marcin Kuthan <marcin.kuthan@gmail.com>
2014-05-16 13:57:32 +0200

    Move shared properties to the configuration module.

--- conference-configuration/src/main/java/example/conference/configuration/ConferenceConfiguration.java
+++ conference-configuration/src/main/java/example/conference/configuration/ConferenceConfiguration.java
@@ -4,20 +4,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:/example/conference/configuration/configuration.properties")
 @ImportResource({
         "classpath:/example/conference/configuration/axon.xml",
         "classpath:/example/conference/configuration/rabbit.xml"
 })
-@EnableConfigurationProperties
 public class ConferenceConfiguration {
-
-    @Bean
-    public ConferenceProperties conferenceProperties() {
-        return new ConferenceProperties();
-    }
-
     @Bean
     public EventAuditListener eventAuditListener() {
         return new EventAuditListener();
